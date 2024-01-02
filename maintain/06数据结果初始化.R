f4b14f34c4a65b3e796057f00eeef61893cca6ae
auptz <936442093@qq.com>
2024-01-02 23:34:54 +0800

    优化数据结果初始化流程

--- maintain/06数据结果初始化.R
+++ maintain/06数据结果初始化.R
@@ -1,4 +1,12 @@
-# 读取resinfo.db文件
+# 删除指定目录下的所有文件
+list_dir = c("cache/SM/","cache/BM/","cache/SS/")
+
+for (i in list_dir){
+  file.remove(paste0(i,dir(i)))
+  dir.create(i)
+}
+
+# 初始化结果数据resinfo.db文件
 library(RSQLite)
 
 # Connect to the SQLite database
