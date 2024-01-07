4bcadc17b991cc83d1ef4a0270565a0d2d3fdbbf
auptz <936442093@qq.com>
2024-01-07 23:38:10 +0800

    优化维护代码运行模式

--- maintain/04使用数据库存储nulldistribution.R
+++ maintain/04使用数据库存储nulldistribution.R
@@ -10,6 +10,7 @@ if(F){
         file.rename(from = ii, to = str_replace_all(ii,c("ss_xsum"="SS_Xsum",
                                                          "ss_cos"="SS_XCos",
                                                          "ss_zh"="SS_ZhangScore",
+                                                         "SS_GESA" = "SS_GSEA",
                                                          "ss_gs"="SS_GESA",
                                                          "ss_ks"="SS_CMap")))
       )
@@ -30,38 +31,33 @@ library(tidyr)
 conn <- dbConnect(RSQLite::SQLite(), "data_preload/nulldistribution/Nulldistribution.db")
 
 
+tables <- dbListTables(conn)
 
-
+dbListTables(conn) %>% purrr::walk(~dbRemoveTable(conn, .))
+# dbExecute(conn, "VACUUM")
+tables <- dbListTables(conn)
 
 dir2 = "NULLprocessing/NULLscore/"
 
-for (i in list.files(dir2)){
+## "LINCS_A375_10 µM_24 h.rdata_SS_Xsum.rdata"
 
-    load(paste0(dir2,i))
-    colnames(nulldistribution) <- 1:ncol(nulldistribution)
-    
-    ddd <- nulldistribution[,1:1999] %>% 
-      rownames_to_column(var = "drugname")
-    
-    dbWriteTable(conn, i, ddd)
-
-    print(paste0("writed ",i))
-
+nn = 0
+for (i in list.files(dir2)[!(list.files(dir2) %in% tables)]){
+      load(paste0(dir2,i))
+      colnames(nulldistribution) <- 1:ncol(nulldistribution)
+      
+      ddd <- nulldistribution[,1:(ncol(nulldistribution) - 1)] %>% 
+        rownames_to_column(var = "drugname")
+      
+      dbWriteTable(conn, i, ddd)
+      nn = nn + 1
+      print(paste0("writed ",i,"  ",nn))
 }
-
-# 用于筛选前1999的signature
-# ddd %>%  dplyr::filter(drugname == "fluspirilene") %>% dplyr::select(-drugname) %>% unlist(use.names = FALSE)
-
-
-
-# 
-
-
-
-
-
 # List all the tables available in the database
 dbListTables(conn)
+dbExecute(conn, "VACUUM")
 # END
 dbDisconnect(conn)
 
+# 用于筛选前1999的signature
+# ddd %>%  dplyr::filter(drugname == "fluspirilene") %>% dplyr::select(-drugname) %>% unlist(use.names = FALSE)
--- maintain/08计算benchmark和applicaition的运行时间.R
+++ maintain/08计算benchmark和applicaition的运行时间.R
@@ -1,78 +1,108 @@
-library(microbenchmark)
+library(bench)
 
-benchmarkTest <- function(profile){
-  library(dplyr)
-  library(tidyr)
-  library(ggplot2)
-  library(tibble)
-  library(pROC)
-  library(rio)
-  library(tidyverse)
+if(F){
+  benchmarkTest <- function(profile){
+    library(dplyr)
+    library(tidyr)
+    library(ggplot2)
+    library(tibble)
+    library(pROC)
+    library(rio)
+    library(tidyverse)
+    
+    setwd("~/rprojects/SSP/")
+    
+    source("R/global.R")
+    source("R/SS.R")
+    source("R/Utils_BM.R")
+    source("R/Utils_RB.R")
+    source("R/Utils_SM.R")
+    
+    IC50_drug <- rio::import("demo/drug_annotation_AUC.txt")
+    FDA_drug <- rio::import("demo/drug_annotation_ES.txt")
+    i.need.logfc <- rio::import("demo/signature.txt") %>% dplyr::select(c("Gene","log2FC"))
+    sel_exp = profile
+    sel_ss = c("SS_Xsum","SS_CMap","SS_GSEA","SS_ZhangScore","SS_XCos")
+    
+    res_bm <- get_benchmark(
+      IC50_drug = IC50_drug,
+      FDA_drug = FDA_drug, 
+      i.need.logfc = i.need.logfc, 
+      sel_exp = sel_exp,
+      sel_ss = sel_ss
+    )
+    
+    source("~/rprojects/SSP/maintain/06数据结果初始化.R")
+    print(paste0("work down! ",Sys.time()))
+  }
   
-  setwd("~/rprojects/SSP/")
+  # 使用microbenchmark()函数来评估函数
+  res_benchmark_dataset_major = bench::mark(benchmarkTest("LINCS_VCAP_5 µM_24 h.rdata"), 
+                                            iterations = 100, check = FALSE )
+  res_benchmark_dataset_minor = bench::mark(benchmarkTest("LINCS_MCF7_1 nM_24 h.rdata"), 
+                                            iterations = 100, check = FALSE)
   
-  source("R/global.R")
-  source("R/SS.R")
-  source("R/Utils_BM.R")
-  source("R/Utils_RB.R")
-  source("R/Utils_SM.R")
-  
-  IC50_drug <- rio::import("demo/drug_annotation_AUC.txt")
-  FDA_drug <- rio::import("demo/drug_annotation_ES.txt")
-  i.need.logfc <- rio::import("demo/signature.txt") %>% dplyr::select(c("Gene","log2FC"))
-  sel_exp = profile
-  sel_ss = c("SS_Xsum","SS_CMap","SS_GESA","SS_ZhangScore","SS_XCos")
-  
-  res_bm <- get_benchmark(
-    IC50_drug = IC50_drug,
-    FDA_drug = FDA_drug, 
-    i.need.logfc = i.need.logfc, 
-    sel_exp = sel_exp,
-    sel_ss = sel_ss
-  )
-  
-  source("~/rprojects/SSP/maintain/06数据结果初始化.R")
-  print(paste0("work down! ",Sys.time()))
+  save(res_benchmark_dataset_major,res_benchmark_dataset_minor,file = "res_benchmark.Rdata")
 }
 
-# 使用microbenchmark()函数来评估函数
-res_benchmark_dataset_large = microbenchmark(benchmarkTest("LINCS_VCAP_5 µM_24 h.rdata"), times = 100)
-res_benchmark_dataset_min = microbenchmark(benchmarkTest("LINCS_MCF7_1 nM_24 h.rdata"), times = 100)
-
-save(res_benchmark_dataset_large,res_benchmark_dataset_min,file = "res_benchmark.Rdata")
 
 
-applicationTest <- function(profile,sel_model_input,topn = 150,direct = "down"){
-  
-  
-  
-  library(dplyr) # 为了future正常运行使用的
-  library(tidyr)
-  library(tidyverse)
-  setwd("~/rprojects/SSP/")
-  
-  source("R/global.R")
-  source("R/SS.R")
-  source("R/Utils_BM.R")
-  source("R/Utils_RB.R")
-  source("R/Utils_SM.R")
-  
-  i.need.logfc <- rio::import("demo/signature.txt") %>% dplyr::select(c("Gene","log2FC"))
-  sel_ss = c("SS_Xsum","SS_CMap","SS_GESA","SS_ZhangScore","SS_XCos")
 
-  
-  
-  
-  res_sm_f <-  get_single_method(drug_profile = profile,
-                                 topn = topn,
-                                 sel_model_sm1= sel_model_input,
-                                 i.need.logfc = i.need.logfc,
-                                 funcname = funcname,
-                                 funcname_mul = funcname_mul,
-                                 direct = direct,
-                                 bioname1 = bioname1,
-                                 bioname2 = bioname2)
-  
-  
+
+if(T){
+  applicationTest <- function(profile,sel_model_input){
+    
+    library(dplyr) # 为了future正常运行使用的
+    library(tidyr)
+    library(tidyverse)
+    setwd("~/rprojects/SSP/")
+    
+    source("R/global.R")
+    source("R/SS.R")
+    source("R/Utils_BM.R")
+    source("R/Utils_RB.R")
+    source("R/Utils_SM.R")
+    
+    i.need.logfc <- rio::import("demo/signature.txt") %>% 
+      dplyr::select(c("Gene","log2FC")) %>% 
+      dplyr::mutate(log2FC = runif(n(), min = -1, max = 1))
+    
+    funcname_mul = c("SS_Xsum","SS_CMap","SS_GSEA","SS_ZhangScore","SS_XCos")
+    funcname = "SS_GSEA"
+    topn = 150
+    direct = "down"
+    bioname1 = "bioname1"
+    bioname2 = "bioname2"
+    
+    res_sm_f <-  get_single_method(drug_profile = profile, # 需要额外设定
+                                   topn = topn,
+                                   sel_model_sm1= sel_model_input, # 需要额外设定
+                                   i.need.logfc = i.need.logfc,
+                                   funcname = funcname,
+                                   funcname_mul = funcname_mul,
+                                   direct = direct,
+                                   bioname1 = bioname1,
+                                   bioname2 = bioname2)
+  }
+
+  res_application_SM_major = bench::mark(applicationTest(profile = "LINCS_VCAP_5 µM_24 h.rdata",
+                                                         sel_model_input = "singlemethod"), 
+                                            iterations = 100, check = FALSE )
+  res_application_SM_minor = bench::mark(applicationTest(profile = "LINCS_MCF7_1 nM_24 h.rdata",
+                                                         sel_model_input = "singlemethod"), 
+                                            iterations = 100, check = FALSE)
+  res_application_ALL_major = bench::mark(applicationTest(profile = "LINCS_VCAP_5 µM_24 h.rdata",
+                                                         sel_model_input = "SS_ALL"), 
+                                         iterations = 100, check = FALSE )
+  res_application_SM_minor = bench::mark(applicationTest(profile = "LINCS_MCF7_1 nM_24 h.rdata",
+                                                         sel_model_input = "SS_ALL"), 
+                                         iterations = 100, check = FALSE)
+  res_application_SM_major = bench::mark(applicationTest(profile = "LINCS_VCAP_5 µM_24 h.rdata",
+                                                         sel_model_input = "SS_all"), 
+                                         iterations = 100, check = FALSE )
+  res_application_SM_minor = bench::mark(applicationTest(profile = "LINCS_MCF7_1 nM_24 h.rdata",
+                                                         sel_model_input = "SS_cross"), 
+                                         iterations = 100, check = FALSE)
   
 }
+
