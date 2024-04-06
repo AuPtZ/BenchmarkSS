75f947e74d34d7d73eb7ed1e3cfd9e6e7d8ef711
auptz <936442093@qq.com>
2024-04-06 14:49:02 +0800

    数据整理

---
+++ maintain/12.将两次分别topN计算的robustness合并.R
@@ -0,0 +1,7 @@
+# 首先获取tmp的文件列表，定义输入和输出的内容
+input_dir = "~/rprojects/SSP/data_preload/robustness/tmp_files"
+output_dir = "~/rprojects/SSP/data_preload/robustness"
+
+# 读取tmp的文件
+file_list <- list.files(input_dir)
+
